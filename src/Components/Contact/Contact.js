d49905a8ef5495379e0622a2e34059b664c48422
Siva Kiran Seelam <Siva@sivas-macbook-pro.local>
2021-07-18 22:59:23 -0400

    First Commit

--- src/Components/Contact/Contact.js
+++ src/Components/Contact/Contact.js
@@ -11,8 +11,8 @@ function Contact() {
         <Container fluid>
           <h1 className="display-3">My Contact Details</h1>
           <p className="lead">Here are my contact details </p>
-          <p className="lead">Email : sachin@wynisco.com my email </p>
-          <p className="lead">Phone : 732 874 0188 </p>
+          <p className="lead">Email : siva.seelam@gmail.com </p>
+          <p className="lead">Phone : 813 368 0974 </p>
           <p className="lead">Typical Wake up / Sleep Hours : Other than bootcamp. I will available morning 7AM to 11AM EST via slack or whatsapp Changed  </p>
         </Container>
       </Jumbotron>
